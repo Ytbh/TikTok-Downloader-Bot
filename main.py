2b8ef5e02b532f621cc272ee6bce26e0f09927c5
Ytbh <89383662+Ytbh@users.noreply.github.com>
2021-12-24 22:49:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash, workers=w
 
 @app.on_message(filters.command('start'))
 def start(client, message):
-    kb = [[InlineKeyboardButton('Channel 🛡', url=https://t.me/projectXDF),InlineKeyboardButton('Repo 🔰', url="https://github.com/TerminalWarlord/TikTok-Downloader-Bot/")]]
+    kb = [[InlineKeyboardButton('Channel 🛡', url="https://t.me/projectXDF/"),InlineKeyboardButton('Repo 🔰', url="https://github.com/TerminalWarlord/TikTok-Downloader-Bot/")]]
     reply_markup = InlineKeyboardMarkup(kb)
     app.send_message(chat_id=message.from_user.id, text=f"Hello there, I am **TikTok Downloader Bot**.\nI can download TikTok video without Watermark.\n\n"
                           "__**Developer :**__ __@YugoslavBoy\n"
